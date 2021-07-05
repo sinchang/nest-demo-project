6f9d0e613f7c8185def49a1aa0edb699f7404605
sinchang <sinchangwen@gmail.com>
2021-07-05 13:40:41 +0800

    read port from env variable

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
 
-  await app.listen(3001, '0.0.0.0');
+  await app.listen(process.env.PORT || 3001, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
